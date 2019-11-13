0023b38634d0749d8918336eb30830f9c3145a54
Kizito Njoku <nkizito57@gmail.com>
2019-11-13 20:25:26 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,40 @@
+class Song 
+  attr_accessor :artist, :genre, :name
+  @@artists = []
+  @@count = 0
+  @@genres = []
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @@count += 1
+    
+    @genre = genre 
+    @@genres << genre
+    
+    @artist = artist
+    @@artists << artist
+  end 
+  
+  def self.genre_count
+    @@genres.inject(Hash.new(0)) {|max, i| max[i]+=1; max}
+  end 
+  
+  def self.genres 
+    @@genres.uniq
+  end 
+  
+  def self.artist_count
+    @@artists.inject(Hash.new(0)) {|max, i| max[i]+=1; max}
+  end 
+  
+   def self.artists 
+     @@artists.uniq
+   end 
+    
+    def self.count 
+      @@count 
+    end 
+    
+end 
+
+
